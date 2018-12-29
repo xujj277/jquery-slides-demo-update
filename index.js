b3386843c091e18eb477c045bff4a84eb3245a15
xujj277 <939160741@qq.com>
2018-12-29 14:08:06 +0800

    domyself

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
+
 let n
 init()
 
-
 setInterval(() => {
-
+  console.log(n)
   makeLeave(getImage(n))
-    .one('transitionend', (e) => {
+    .one('transitionend', function (e) {
       makeEnter($(e.currentTarget))
     })
   makeCurrent(getImage(n+1))
   n += 1
 }, 3000)
 
-function getImage (n) {
-  return $(`.images > img:nth-child(${x(n)})`)
+
+function getImage (i) {
+  return $(`.images>img:nth-child(${modifyImagesNumber(i)})`)
 }
 
-function x (n) {
-  if (n>3) {
+function init () {
+  n = 1
+  $(`.images>img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+}
+
+function modifyImagesNumber (n) {
+  if (n > 3) {
     n = n % 3
     if (n === 0) {
       n = 3
@@ -26,19 +32,15 @@ function x (n) {
   return n
 }
 
-function init () {
-  n = 1
-  $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
-}
-
-function makeCurrent ($node) {
-  return $node.removeClass('enter leave').addClass('current')
-}
 
 function makeLeave ($node) {
-  return $node.removeClass('enter current').addClass('leave')
+  return $node.removeClass('current').addClass('leave')
 }
 
 function makeEnter ($node) {
-  return $node.removeClass('leave current').addClass('enter')
+  return $node.removeClass('leave').addClass('enter')
+}
+
+function makeCurrent ($node) {
+  return $node.removeClass('enter').addClass('current')
 }
