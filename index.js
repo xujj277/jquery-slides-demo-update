92d06bc88ada9ce7e7643e5e86073fb642ff7fb9
xujj277 <939160741@qq.com>
2018-12-29 10:55:50 +0800

    优化代码

--- index.js
+++ index.js
@@ -2,17 +2,23 @@ $('.images > img:nth-child(1)').addClass('current')
 $('.images > img:nth-child(2)').addClass('enter')
 $('.images > img:nth-child(3)').addClass('enter')
 
-let n = 1
+let n
+init()
+
 
 setInterval(() => {
 
-  $(`.images > img:nth-child(${x(n)})`).removeClass('current').addClass('leave')
+  makeLeave(getImage(n))
     .one('transitionend', (e) => {
-      $(e.currentTarget).removeClass('leave').addClass('enter')
+      makeEnter($(e.currentTarget))
     })
-  $(`.images > img:nth-child(${x(n+1)})`).removeClass('enter').addClass('current')
+  makeCurrent(getImage(n+1))
   n += 1
 }, 3000)
+
+function getImage (n) {
+  return $(`.images > img:nth-child(${x(n)})`)
+}
 
 function x (n) {
   if (n>3) {
@@ -22,4 +28,22 @@ function x (n) {
     }
   }
   return n
+}
+
+function init () {
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current').siblings().addClass('enter')
+}
+
+function makeCurrent ($node) {
+  $node.removeClass('enter leave').addClass('current')
+}
+
+function makeLeave ($node) {
+  $node.removeClass('enter current').addClass('leave')
+  return $node
+}
+
+function makeEnter ($node) {
+  $node.removeClass('leave current').addClass('enter')
 }
